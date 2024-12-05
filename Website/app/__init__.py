2742ac1491bc3fb720003d9c63f2668e37078ae7
KhiryDixonManning <Manningkhiry@gmail.com>
2024-12-05 12:56:43 -0600

    changed init and config file to match data with other files

--- Website/app/__init__.py
+++ Website/app/__init__.py
@@ -1,7 +1,8 @@
 import firebase_admin
-from firebase_admin import credentials, firestore
+from firebase_admin import credentials, firestore, db
 from flask import Flask
 from flask_cors import CORS
+import os
 
 def create_app():
     app = Flask(__name__)
@@ -18,11 +19,27 @@ def create_app():
     })
     
     # Initialize Firebase
-    cred = credentials.Certificate('config/firebase_credentials.json')
-    firebase_admin.initialize_app(cred)
+    cred = credentials.Certificate({
+        "type": "service_account",
+        "project_id": "text-adventure-55f7e",
+        "private_key_id": "5854172ebdfec209f06e7509824233e6dbf3c1ba",
+        "private_key": os.environ.get('FIREBASE_PRIVATE_KEY', '').replace('\\n', '\n'),
+        "client_email": "firebase-adminsdk-tyi4b@text-adventure-55f7e.iam.gserviceaccount.com",
+        "client_id": "118190399587024702849",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-tyi4b%40text-adventure-55f7e.iam.gserviceaccount.com"
+    })
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': 'https://text-adventure-55f7e-default-rtdb.firebaseio.com/'
+    })
     
-    # Initialize Firestore
-    db = firestore.client()
-    app.config['FIRESTORE'] = db
+    # Initialize Firestore and Realtime Database
+    firestore_db = firestore.client()
+    realtime_db = db.reference('/')
+    
+    app.config['FIRESTORE'] = firestore_db
+    app.config['REALTIME_DB'] = realtime_db
     
     return app
--- Website/app/config.py
+++ Website/app/config.py
@@ -5,9 +5,12 @@ load_dotenv()
 
 class Config:
     # Web-specific configuration
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    ALLOWED_WEBSITE = os.environ.get('ALLOWED_WEBSITE', 'https://yourwebsite.com')
-    FIREBASE_CREDENTIALS = os.environ.get('FIREBASE_CREDENTIALS_PATH')
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key')
+    ALLOWED_WEBSITE = 'https://zytronium.github.io'
+    
+    # Firebase Configuration
+    FIREBASE_PROJECT_ID = 'text-adventure-55f7e'
+    FIREBASE_REALTIME_DB_URL = 'https://text-adventure-55f7e-default-rtdb.firebaseio.com/'
     
     # JWT Configuration
     JWT_ALGORITHM = 'HS256'
