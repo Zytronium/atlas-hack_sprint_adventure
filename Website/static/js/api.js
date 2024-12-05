7b0680bd523cba292ae088fc240eee72ed854011
KhiryDixonManning <Manningkhiry@gmail.com>
2024-12-05 12:34:38 -0600

    added url to api base url

--- Website/static/js/api.js
+++ Website/static/js/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://zytronium.github.io/atlas-hack_sprint_adventure/';
 
 async function saveGameState(gameData) {
     try {
